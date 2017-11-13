70d57df9f57dbb4f22c7bb711ee2976590ab8587
aijunx <aijun198600@126.com>
2017-11-13 17:48:23 +0800

    发布0.0.15版本

--- src/index.js
+++ src/index.js
@@ -2,21 +2,36 @@
  * Created by Aijun on 2017/11/10.
  */
 
+import Vue from 'vue'
+
 import DateUtil from './DateUtil'
-import BaseButton from './v-base-button.vue'
 
 const components = {
-
+    "v-base-button":require('./v-base-button.vue')
 };
 
 const utils = {
-
+    DateUtil:DateUtil,
 };
 
-const API = {
-    DateUtil:DateUtil,
+const install = function (Vue, opts = {}) {
+    Object.keys(components).forEach(key => {
+        Vue.component(key, components[key]);
+    });
 
-    BaseButton:require('./v-base-button.vue')
+    Vue.prototype.$utils = utils;
+};
+
+//auto install
+if (typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue);
+}
+
+const API = {
+    version: '0.0.15',
+    install,
+    ...components,
+    ...utils
 };
 
 export default API;
--- src/v-base-button.vue
+++ src/v-base-button.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=".base-button">
+    <div class="base-button">
     </div>
 </template>
 
